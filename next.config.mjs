02a28d9391695324aa8eb89519dbfe55a2a6b1f3
unknown <0xBlyn>
2024-12-10 19:46:55 -0800

    Fixing Bugs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         unoptimized: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals = [...(config.externals || []), '@prisma/client']
+        }
+        return config
     }
 };
 
