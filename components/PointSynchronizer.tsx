09691aaa3e9fd64f9083f783f60804e0bbe98aed
unknown <0xBlyn>
2024-12-10 22:06:06 -0800

    Bug fixes

--- components/PointSynchronizer.tsx
+++ components/PointSynchronizer.tsx
@@ -42,34 +42,25 @@ export function PointSynchronizer() {
             console.log("Data from server: ", data);
             const updatedUnsynchronizedPoints = Math.max(0, unsynchronizedPoints - frozenPointsToSynchronized);
 
-            // initializeState({
-            //     points: data.points,
-            //     pointsBalance: data.pointsBalance,
-            //     energy: data.energy,
-            //     unsynchronizedPoints: updatedUnsynchronizedPoints,
-            // });
             showSuccessMessage(`Successfully synchronized! New points to synchronize: ${updatedUnsynchronizedPoints}`);
         } catch (error) {
             showErrorMessage('Error syncing with server:');
             console.error('Error syncing with server:', error);
         }
-    }, [userTelegramInitData, unsynchronizedPoints, energy, initializeState]);
+    }, [userTelegramInitData, unsynchronizedPoints, energy]);
 
     useEffect(() => {
-        // Clear any existing timeout
         if (syncTimeoutRef.current) {
             clearTimeout(syncTimeoutRef.current);
         }
 
         console.log("unsynchronizedPoints", unsynchronizedPoints);
-        // Set a new timeout
         syncTimeoutRef.current = setTimeout(() => {
             if (unsynchronizedPoints > 1) {
                 syncWithServer();
             }
         }, 2000);
 
-        // Cleanup function
         return () => {
             if (syncTimeoutRef.current) {
                 clearTimeout(syncTimeoutRef.current);
